b4411d163a9fd6062d241ac2084ff1aca1b90faa
Cade <phillipscb@hendrix.edu>
2021-03-21 17:34:33 -0500

    Main Menu mostly working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public GameObject events;
+    public GameObject instructionsButton;
+    public GameObject startButton;
+    public GameObject sourcesButton;
+    public GameObject canvas;
 
+    public GameObject title;
+    public GameObject backgroundImage;
+    public GameObject sourcesText;
+    public GameObject instructionsText;
     public List<LightColor> bulbInventory = new List<LightColor>();
 
     private void Awake()
@@ -41,4 +50,39 @@ public class GameManager : MonoBehaviour
             bulbInventory.Add(color);
         }
     }
-}
+
+    public void InstructionsButton()
+    {
+
+       
+        
+        instructionsButton.SetActive(false);
+        sourcesButton.SetActive(true);
+        sourcesText.GetComponent<TextMeshProUGUI>().text = "";
+        instructionsText.GetComponent<TextMeshProUGUI>().text = "Find your bulb";
+
+
+    }
+    public void StartButton()
+    {
+
+        //StartCoroutine(ColorLerp(new Color(1, 1, 1, 0), 2,backgroundImage));
+        startButton.SetActive(false);
+        instructionsButton.SetActive(false);
+        sourcesButton.SetActive(false);
+        title.SetActive(false);
+        instructionsText.GetComponent<TextMeshProUGUI>().text = "";
+        sourcesText.GetComponent<TextMeshProUGUI>().text = "";
+
+       
+    }
+    public void SourcesButton()
+    {
+
+        instructionsButton.SetActive(true);
+        sourcesButton.SetActive(false);
+        instructionsText.GetComponent<TextMeshProUGUI>().text = "";
+        sourcesText.GetComponent<TextMeshProUGUI>().text = "There will be some here";
+    }
+
+    }
