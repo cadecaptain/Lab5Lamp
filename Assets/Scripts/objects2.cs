0f8cd3d49bd133563923e5fcfca96557688130f8
Cade <phillipscb@hendrix.edu>
2021-03-23 16:12:27 -0500

    had to make new script bc i couldnt reattach the old one. deleted specific scripts

--- Assets/Scripts/objects2.cs
+++ Assets/Scripts/objects2.cs
@@ -2,22 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GreenObjects : MonoBehaviour
+public class objects2 : MonoBehaviour
 {
+    public LightColor color;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha4) && GameManager.Instance.CheckInventory(LightColor.Green))
+        if (GameManager.Instance.activeColor == color)
         {
             transform.GetComponent<MeshRenderer>().enabled = true;
         }
-        if (Input.GetKeyDown(KeyCode.Alpha1) || (Input.GetKeyDown(KeyCode.Alpha2) && GameManager.Instance.CheckInventory(LightColor.BlackLight)) || (Input.GetKeyDown(KeyCode.Alpha3) && GameManager.Instance.CheckInventory(LightColor.Infrared)))
+        else
         {
             transform.GetComponent<MeshRenderer>().enabled = false;
         }
