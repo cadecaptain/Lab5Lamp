4905d2752977459563b33dc18969f66d93d2284d
Cameron Minor <minorcj@hendrix.edu>
2021-03-23 20:44:02 -0500

    Puzzle Hidden by blacklight

--- Assets/Scripts/ColorPads.cs
+++ Assets/Scripts/ColorPads.cs
@@ -8,9 +8,16 @@ public class ColorPads : MonoBehaviour
     // 1 = blue
     // 2 = green
     public int color;
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        audio.Play();
         GameManager.Instance.CreateCode(color);
         Debug.Log("Added " + color + " to the code");
     }
